6e9a7a3531fc238c5311cc7fe8d8f06229094f44
HakkiNasiboglu <84343562+HakkiNasiboglu@users.noreply.github.com>
2025-02-20 14:01:57 +0300

    Create myFirstLabScript.py

---
+++ myFirstLabScript.py
@@ -0,0 +1,31 @@
+name = input("What is your name?")
+print("Hello " + name + ".")
+id = input("What is your id number?")
+print("Your ID is " + id + ".")
+
+var1 = int(input("Enter var1:"))
+var2 = int(input("Enter var2:"))
+
+sum = (var1 + var2)
+diff = (var1 / var2)
+prod = (var1 * var2)
+
+print(var1)
+print(var2)
+print(sum)
+print(diff)
+print(prod)
+
+student = input("What is your name?")
+lab = int(input("Enter your lab score:"))
+midterm = int(input("Enter your midterm score:"))
+final = int(input("Enter your final score:"))
+LastScore = (lab * 25/100) + (midterm * 35/100) + (final * 40/100)
+print(student)
+print(lab)
+print(midterm)
+print(final)
+print(LastScore)
+
+print("*\n**\n***\n**\n*")
+
